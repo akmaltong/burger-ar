3a964a3129002c6a45df4a116bea9fcc635c29d5
Akmaltong <40340558+akmaltong@users.noreply.github.com>
2026-02-11 02:25:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
 const modelViewer = document.querySelector('#burger-model');
 
-// Поддержка смены бургеров из твоего меню
 const urlParams = new URLSearchParams(window.location.search);
-const burgerId = urlParams.get('burger'); // теперь ловим ?burger=1 и т.д.
+const modelName = urlParams.get('model');
 
-// Карта моделей
-const models = {
-    '0': 'Burger.glb',
-    '1': 'Big Burger.glb',
-    '2': 'Zinger burger.glb',
-    '3': 'Hamburger.glb',
-    '4': 'Hamburger2.glb',
-    '5': 'Burger low.glb'
-};
-
-if (burgerId && models[burgerId]) {
-    modelViewer.src = models[burgerId];
+if (modelName) {
+    modelViewer.src = `${modelName}.glb`;
+    modelViewer.poster = `${modelName}.webp`;
 }
 
-// ПРИНУДИТЕЛЬНО отключаем WebXR для стабильности
-modelViewer.addEventListener('load', () => {
-    modelViewer.arModes = "scene-viewer";
-});
-
 modelViewer.addEventListener('progress', (event) => {
-    const progressBar = document.querySelector('.progress-bar');
-    const updatingBar = document.querySelector('.update-bar');
-    updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-    if (event.detail.totalProgress === 1) {
-        progressBar.classList.add('hide');
-    }
+  const progressBar = document.querySelector('.progress-bar');
+  const updatingBar = document.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+  }
 });
