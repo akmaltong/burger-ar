308fad0f93d032bd83251063751aa4de9077aff0
Akmaltong <40340558+akmaltong@users.noreply.github.com>
2026-02-10 23:22:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,22 @@
-:not(:defined) > * {
-  display: none;
+const modelViewer = document.querySelector('#burger-model');
+
+// Магия QR: меняем модель, если в ссылке есть ?model=Название
+const urlParams = new URLSearchParams(window.location.search);
+const modelName = urlParams.get('model');
+
+if (modelName) {
+    modelViewer.src = `${modelName}.glb`;
+    modelViewer.poster = `${modelName}.webp`;
 }
 
-body, html {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100%;
-  background-color: transparent; /* Делаем фон страницы прозрачным */
-}
-
-model-viewer {
-  width: 100%;
-  height: 100vh;
-  background-color: transparent; /* Делаем фон вьюера прозрачным */
-  --poster-color: transparent;
-}
-
-/* Стили из твоего файла для кнопок и прогресс-бара */
-.progress-bar {
-  display: block;
-  width: 33%;
-  height: 2%;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate3d(-50%, -50%, 0);
-  border-radius: 25px;
-  box-shadow: 0px 3px 10px 3px rgba(0, 0, 0, 0.5);
-  background-color: rgba(0, 0, 0, 0.5);
-}
-
-.progress-bar.hide { visibility: hidden; transition: visibility 0.3s; }
-
-.update-bar {
-  background-color: rgba(255, 255, 255, 0.9);
-  width: 0%;
-  height: 100%;
-  border-radius: 25px;
-  transition: width 0.3s;
-}
-
-#ar-button {
-  background-color: #fff;
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  bottom: 16px;
-  padding: 0px 16px;
-  font-family: Roboto Regular, Helvetica Neue, sans-serif;
-  font-size: 14px;
-  color:#4285f4;
-  height: 36px;
-  line-height: 36px;
-  border-radius: 18px;
-  border: 1px solid #DADCE0;
-  cursor: pointer;
-}
+// Полоска загрузки
+modelViewer.addEventListener('progress', (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+  } else {
+    progressBar.classList.remove('hide');
+  }
+});
