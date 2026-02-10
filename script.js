509921ee4fc191a4f5f74dd079313453a6f84230
Akmaltong <40340558+akmaltong@users.noreply.github.com>
2026-02-10 23:30:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-const modelViewer = document.querySelector('#burger-model');
-
-// Магия QR: меняем модель, если в ссылке есть ?model=Название
-const urlParams = new URLSearchParams(window.location.search);
-const modelName = urlParams.get('model');
-
-if (modelName) {
-    modelViewer.src = `${modelName}.glb`;
-    modelViewer.poster = `${modelName}.webp`;
-}
-
-// Полоска загрузки
-modelViewer.addEventListener('progress', (event) => {
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
   const progressBar = event.target.querySelector('.progress-bar');
   const updatingBar = event.target.querySelector('.update-bar');
   updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
   if (event.detail.totalProgress === 1) {
     progressBar.classList.add('hide');
+    event.target.removeEventListener('progress', onProgress);
   } else {
     progressBar.classList.remove('hide');
   }
-});
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
