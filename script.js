6213a1646cd3147b5e639063bbca09bdbdf05644
Akmaltong <40340558+akmaltong@users.noreply.github.com>
2026-02-11 00:37:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const modelViewer = document.querySelector('#burger-model');
 
-// Поддержка QR-ссылок: ?model=ИмяФайла
 const urlParams = new URLSearchParams(window.location.search);
 const modelName = urlParams.get('model');
 
@@ -9,17 +8,11 @@ if (modelName) {
     modelViewer.poster = `${modelName}.webp`;
 }
 
-// Полоска загрузки
 modelViewer.addEventListener('progress', (event) => {
   const progressBar = document.querySelector('.progress-bar');
   const updatingBar = document.querySelector('.update-bar');
-  const progress = event.detail.totalProgress;
-  
-  updatingBar.style.width = `${progress * 100}%`;
-  
-  if (progress === 1) {
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
     progressBar.classList.add('hide');
-  } else {
-    progressBar.classList.remove('hide');
   }
 });
